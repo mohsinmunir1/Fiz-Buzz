0d1d28493127aaf230fa9053ded23908fbd59485
Mohsin <abdul.mohsin.malik@gmail.com>
2020-05-16 02:12:31 +0500

    vuejs configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+Vue.use(VueAxios, axios);
+
+Vue.component('test-component', require('./components/TestComponent.vue').default);
+
+const app = new Vue().$mount('#app');
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>dello</div>
+</template>
+
+<script>
+  export default {
+    name: "TestComponent.vue",
+    created() {
+      axios
+        .get('/api/random')
+        .then(response => (this.info = response))
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
